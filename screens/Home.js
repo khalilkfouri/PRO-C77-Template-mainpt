79cd464ca098dd660fa14740d0bd32ccf7295105
khalilkfouri <96916170+khalilkfouri@users.noreply.github.com>
2022-11-07 18:33:13 -0300

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -26,7 +26,7 @@ export default class HomeScreen extends Component {
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
                 */}
-                {/* <ImageBackground 
+                <ImageBackground 
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
                 */}
@@ -52,7 +52,7 @@ export default class HomeScreen extends Component {
                           style={styles.routeCard} onPress={() =>
                         this.props("SpaceCraft")
                     }> */}
-                    {/* <TouchableOpacity 
+                     <TouchableOpacity 
                           style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("SpaceCraft")
                     }> */}                    
